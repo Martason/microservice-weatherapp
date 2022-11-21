9d7339e91dc032eb4ae55f5b792dd5767492dfa4
Martason <annamarta.hjalmarson@utb.ecutbildning.se>
2022-11-21 12:35:05 +0100

    added inital deps

--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
+
+app.MapGet("/observation/{town}", (string town, [FromQuery] int? days) =>
+{
+    return Results.Ok(town);
+});
 
 app.Run();
 
